cecac654d59ccd9c4da685adc3904003bfd669f0
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-09-21 16:15:02 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,39 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.02,
+            length: 20
+        }
+        
+        this.slingShot = Constraint.create(options);
+        this.pointB=pointB;
+
+        World.add(world,this.slingShot);
+
+
+       console.log(this.slingShot)
+    }
+
+    fly(){
+       this.slingShot.bodyA=null;
+    }
+
+    attach(body){
+        this.slingShot.bodyA=body;
+
+    }
+
+    
+
+
+    display(){
+    if(this.slingShot.bodyA){
+        var pointA=this.slingShot.bodyA.position;
+        var pointB=this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y)
+       }
+    }
+}
